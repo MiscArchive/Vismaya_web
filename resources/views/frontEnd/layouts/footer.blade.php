becd06dab2a68b6e011787611090e1b6d3232309
AnuragCkMeridian <anurag@meridian.net.in>
2023-11-15 22:41:17 +0530

    footer-changes

--- resources/views/frontEnd/layouts/footer.blade.php
+++ resources/views/frontEnd/layouts/footer.blade.php
@@ -58,11 +58,11 @@
                   <div class="footer-widget">
                       <h5 class="text-white mb-4">Follow US</h5>
                       <ul class="footer-nav">
-                        <li><a href="https://www.facebook.com/profile.php?id=100094101612222" target="_blank"><i class="fa-brands fa-facebook-f"></i> Facebook</a></li>
-                        <li><a href="https://www.instagram.com/vismaya.hypermarket" target="_blank"><i class="fa-brands fa-instagram"></i> Instagram</a></li>
+                        <li><a href="https://www.facebook.com/profile.php?id=100094101612222" target="_blank"><i class="fa-brands fa-facebook-f"></i><span style="margin-left: 5px;">Facebook</span> </a></li>
+                        <li><a href="https://www.instagram.com/vismaya.hypermarket" target="_blank"><i class="fa-brands fa-instagram"></i><span style="margin-left: 5px;"> Instagram</span></a></li>
                         {{-- <li><a href="#"><i class="fas fa-envelope"></i> Gmail</a></li> --}}
-                        <li><a href="https://www.linkedin.com/in/vismaya-groups-76527a281" target="_blank"><i class="fa-brands fa-linkedin"></i>Linkedin</a></li>
-                        <li><a href="https://twitter.com/vismaya948" target="_blank"><i class="fa-brands fa-twitter"></i>Twitter</a></li>
+                        <li><a href="https://www.linkedin.com/in/vismaya-groups-76527a281" target="_blank"><i class="fa-brands fa-linkedin"></i><span style="margin-left: 5px;">Linkedin</span></a></li>
+                        <li><a href="https://twitter.com/vismaya948" target="_blank"><i class="fa-brands fa-twitter"></i><span style="margin-left: 5px;">Twitter</span></a></li>
                       </ul>
 
                       </ul>
